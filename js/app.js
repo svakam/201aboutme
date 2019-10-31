fe2599afdd3775fb290a7292a84beb7911ad8506
Vik Akam <vikramakam@hotmail.com>
2019-10-31 00:37:59 -0700

    final week 1 day 3 additions to html/css/js. fully functional from appearance

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 'use strict';
 
+// week 1 day 2
 // personalized welcome message to user from me - done
 // prompt user for 5 yes/no questions
 // user must respond y/n yes/no
 // alert user with response they provided
 // display name back to user, display name back to user in final message to them
 
+
 // ask for name
 var name = prompt("What is your name?");
 
 // personalized welcome to user from me, includes question 1 y/n
-var answer1 = prompt("Welcome to my website, " + name + "! My name is Vik Akam, and I'd like to ask you 5 yes/no questions before you enter my website. Question 1: Do you mind if we get started? y/n or yes/no");
+var answer1 = prompt("Welcome to my website, " + name + "! My name is Vik Akam, and I'd like to ask you 7 questions before you enter my website. Question 1: Do you mind if we get started? y/n or yes/no");
 
 // lower case answer 1
 answer1 = answer1.toLowerCase();
@@ -38,11 +40,11 @@ answer2 = answer2.toLowerCase();
 // if 2 yes, say correct; if no, educate
 if (answer2 === "yes" || answer2 === "y") {
   console.log("You said yes; you're correct!");
-  alert("You said yes; you're correct! I love my rock and roll. We come from the land of the ice and snow, from the midnight sun, where the hot springs flow!");
+  alert("You said yes; you're correct! I love my rock and roll man.");
 }
 else if (answer2 === "no" || answer2 === "n") {
-  console.log("You said no. Sorry! I've been playing for a little over 10 years.");
-  alert("You said no. Sorry! I've been playing for a little over 10 years.");
+  console.log("You said no. Sorry! I've been playing for a little over 10 years! Time flies.");
+  alert("You said no. Sorry! I've been playing for a little over 10 years! Time flies.");
 }
 else {
   alert("???");
@@ -70,15 +72,15 @@ else {
 }
 
 // question 4 y/n
-var answer4 = prompt("Question 4: Do I have a younger brother?");
+var answer4 = prompt("Question 4: Do I have a younger brother? y/n or yes/no");
 
 // lower case answer 4
 answer4 = answer4.toLowerCase();
 
 // if 4 yes, correct; if no, educate
 if (answer4 === "yes" || answer4 === "y") {
-  console.log("You replied yes. Correct! We are a 10 year difference. Crazy!");
-  alert("You replied yes. Correct! We are a 10 year difference. Crazy!");
+  console.log("You replied yes. Correct! We are a 10 year difference.");
+  alert("You replied yes. Correct! We are a 10 year difference.");
 }
 else if (answer4 === "no" || answer4 === "n") {
   console.log("You replied no. Wrong - he's younger by 10 years.");
@@ -90,7 +92,7 @@ else {
 }
 
 // question 5 y/n
-var answer5 = prompt("Question 5: Do I love the show Breaking Bad?");
+var answer5 = prompt("Question 5: Do I love the show Breaking Bad?y/n or yes/no");
 
 // lower case answer 5
 answer5 = answer5.toLowerCase();
@@ -98,7 +100,7 @@ answer5 = answer5.toLowerCase();
 // if 5 yes, correct; if no, educate
 if (answer5 === "yes" || answer5 === "y") {
   console.log("Replied yes");
-  alert("You replied yes. Correct! No more half measures, Walter. Go watch the new Breaking Bad movie on Netflix, 'El Camino'!");
+  alert("You replied yes. Correct! No more half measures, Walter.");
 }
 else if (answer5 === "no" || answer5 === "n") {
   console.log("Replied no");
@@ -114,3 +116,137 @@ console.log("final message");
 alert("Thanks for answering (or not answering) my questions, " + name + "! Enjoy my website.");
 
 
+
+
+
+// week 1 day 3
+
+
+// 6th question:
+// while input is not numeric input, prompt user for numeric input
+// 4 guesses allowed
+// if guess incorrect, alert must say too high or too low
+// after attempts exhausted, tell user correct answer
+
+// prep for question 6
+alert("Wait, I almost forgot! Time for some guessing games! >:) Question 6: You have 4 guesses to guess a number from a random number generator.")
+
+// initialize some variables for prepping guessing number game: guessesleft, guessnumber, correctnumber
+var guessesLeft = 4;
+var guessNumber;
+var correctNumber;
+
+// generate random float, multiply by 10, and round down to generate random number
+var randomNumberZeroToOne = Math.random();
+console.log("Random number 0-1 generated: " + randomNumberZeroToOne);
+correctNumber = Math.ceil((10 * (randomNumberZeroToOne)));
+console.log("Correct number: " + correctNumber);
+
+// prompt user at least once for a guess
+do {
+
+  // prompt for input
+  guessNumber = prompt("Take your guess! 1-10 only");
+
+  // convert number to integer
+  guessNumber = parseInt(guessNumber);
+  console.log("guessNumber: " + guessNumber);
+
+  // if guess too low, alert too low and increment guesses, if guess too high, alert too high and increment guesses, if NaN, alert ?, else alert correct and break out of do/while
+  if (guessNumber < correctNumber) {
+    guessesLeft--;
+    alert("Too low! " + guessesLeft + " guesses remaining.");
+  }
+  else if (guessNumber > correctNumber) {
+    guessesLeft--;
+    alert("Too high! " + guessesLeft + " guesses remaining.");
+  }
+  else if (Number.isNaN(guessNumber) === true) {
+    guessesLeft--;
+    alert("???")
+  }
+  else {
+    alert("NICE GUESS!!! You guessed " + guessNumber + ".");
+    break;
+  }
+
+  if (guessesLeft === 0) {
+    alert("Sorry! Out of guesses. The correct number is " + correctNumber);
+    break;
+  }
+
+}
+while (guessNumber !== correctNumber && guessesLeft > 0);
+
+
+
+alert("Glad you got that out of the way, huh? Time for ANOTHER game. >:)");
+
+
+
+// 7th question:
+// multiple correct answers stored in an array
+// 6 guesses allowed to guess correct answer
+// ends when user guesses correct answer or if run out of attempts
+// after, display all correct answers
+
+alert("Question 7: This time, you're going to guess one of my top 10 favorite bands/artists.");
+
+// initialize array of bands, guesses left, and flag to break out
+var favoriteBandsList = ["The Beatles", "Jimi Hendrix", "Led Zeppelin", "Van Halen", "Queen", "Eric Clapton", "Tame Impala", "Elton John", "Billy Joel", "Muse"];
+var bandGuessesLeft = 6;
+console.log("array band list length: " + favoriteBandsList.length);
+for (var i = 0; i < favoriteBandsList.length; i++) {
+  console.log("band name at array[" + i + "]: " + favoriteBandsList[i]);
+}
+var correctGuessExit = false;
+
+do {
+  // prompt for favorite band
+  var guessBand = prompt("What's one of my favorite bands? " + bandGuessesLeft + " guesses remaining.");
+
+  // lowercase input string for checking answer
+  var guessBandLowerCase = guessBand.toLowerCase();
+
+  // more initializing variables
+  var favoriteBandLowerCase;
+  var i;
+
+  // run through array to check string match between guess and array; if match, switch flag to true and break out; else try again and increment guesses
+  for (i = 0; i < favoriteBandsList.length; i++) {
+
+    var ithBand = favoriteBandsList[i];
+    favoriteBandLowerCase = ithBand.toLowerCase();
+
+    console.log(".tolowercase applied to ith band: " + ithBand.toLowerCase());
+    console.log("variable for lower cased favorite band[i]: " + favoriteBandLowerCase);
+
+    if (guessBandLowerCase === favoriteBandLowerCase) {
+
+      correctGuessExit = true;
+      console.log("response correct: flag changed?" + correctGuessExit);
+
+      alert("NICE! I love " + favoriteBandsList[i] + ". Rock on!");
+      break;
+
+    }
+  }
+
+  // if flag is shown to be true, break out of loop entirely; else redo the loop
+  if (correctGuessExit === true) {
+    break;
+  }
+  else {
+    alert("Nope, try again!");
+    bandGuessesLeft--;
+    console.log("guesses left: " + bandGuessesLeft);
+  }
+
+  // if out of guesses, break out of loop
+  if (bandGuessesLeft === 0) {
+    alert("Sorry, out of guesses!");
+    break;
+  }
+
+}
+while (guessesLeft > 0 && favoriteBandLowerCase !== guessBandLowerCase);
